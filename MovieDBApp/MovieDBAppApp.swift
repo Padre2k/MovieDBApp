edd4ad38a672ab0f51d3f41e4d06179af829c5c6
Ethan Faggett <ethanf6@gmail.com>
2025-08-23 23:20:56 -0400

    Initial Commit

---
+++ MovieDBApp/MovieDBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBAppApp.swift
+//  MovieDBApp
+//
+//  Created by Ethan Faggett on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
